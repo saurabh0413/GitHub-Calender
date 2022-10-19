204b56d87830365ef8f8294a5aa6cdb17bb19d66
saurabh <saurabhfarkade83@gmail.com>
2022-10-19 13:13:26 +0530

    github calender added

---
+++ Calender/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calender from "./Components/Calender";
+
+function App() {
+  return (
+    <div className="App">
+      <Calender />
+    </div>
+  );
+}
+
+export default App;
---
+++ Calender/src/Components/Calender.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import ReactTooltip from "react-tooltip";
+const Calender = () => {
+  const selectLastHalfYear = (contributions) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 10;
+
+    return contributions.filter((day) => {
+      const date = new Date(day.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+  return (
+    <div>
+      <GitHubCalendar
+        style={{ margin: "auto" }}
+        username="saurabh0413"
+        transformData={selectLastHalfYear}
+        blockSize={20}
+        fontSize={20}
+      >
+        <ReactTooltip delayShow={20} html />
+      </GitHubCalendar>
+    </div>
+  );
+};
+
+export default Calender;
